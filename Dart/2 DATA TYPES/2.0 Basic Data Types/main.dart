9dc55c7626f4fc7ac9d1361106990b710a35dd3b
Dongmin <pdm001125@naver.com>
2023-09-18 15:07:50 +0900

    Basic Data Types

--- Dart/2 DATA TYPES/2.0 Basic Data Types/main.dart
+++ Dart/2 DATA TYPES/2.0 Basic Data Types/main.dart
@@ -1,4 +1,15 @@
 void main() {
-  // Hello World
-  print('hello world');
+  // Basic Data Type
+
+  // String은 ''과 "" 상관없음
+  String name = 'dongmin';
+  String Name = "Dongmin";
+
+  bool alive = true;
+  int age = 12;
+  double money = 69.99;
+
+  // num이 int와 double의 부모 클래스임 -> 둘 다 할당 될 수 있음. 물론 잘 쓰진 않는다.
+  num x = 12;
+  x = 1.1;
 }
