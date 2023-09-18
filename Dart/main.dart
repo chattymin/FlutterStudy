0109a0cb596ff274955ead8c8cb17457c14854d2
Dongmin <pdm001125@naver.com>
2023-09-18 13:01:11 +0900

    Nullable Variables

--- Dart/main.dart
+++ Dart/main.dart
@@ -24,4 +24,16 @@ void main() {
     // 예를 들어, temp. 으로 접근할 경우 if문 내부에서는 temp가 String이라고 가정하고 String타입의 내장 함수가 나옴
   }
   // Dynamic 타입은 가능한 한 사용하지 않는 것을 권장
+
+  // Nullable Variables
+  String nico = 'nico';
+  // nico = null -> error
+  // why? 일반적인 변수는 null 타입 할당이 불가능하다. 그렇기 때문에 null 타입이 가능한 변수는 따로 표시를 해줘야 한다.
+  String? Nico = 'Nico';
+  Nico = null;
+  // 이때 Nico를 사용하기 위해서는 null일 수도 있기 때문에 null인지 체크 후 사용해야 한다.
+  if (Nico != null) {
+    nico.isEmpty;
+  }
+  Nico?.isNotEmpty; // Nico가 null이 아닐때만 값을 전달해줌. 위의 if문과 같은 내용
 }
