76111a0b8a8c418f9888cf6e6e2fa6a1000fa4cc
Dongmin <pdm001125@naver.com>
2023-09-18 12:32:56 +0900

    Var Keyword 학습

--- Dart/main.dart
+++ Dart/main.dart
@@ -1,3 +1,12 @@
 void main() {
+  // Hello World
   print('hello world');
+
+  // The Var Keyword
+  var naem = '동민'; // 함수나 메소드 내부에서 지역변수 선언시 주로 사용
+  String name = '동민'; // class변수, property 선언시 주로 사용
+
+  naem = '동민동민';
+  name = '동민 동민';
+  // 어떤식으로 선언했든 수정 가능함.
 }
