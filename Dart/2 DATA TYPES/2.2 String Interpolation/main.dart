64788dc890ecc24f067b1efbd883612e09954df3
Dongmin <pdm001125@naver.com>
2023-09-18 17:32:26 +0900

    String Interpolation

---
+++ Dart/2 DATA TYPES/2.2 String Interpolation/main.dart
@@ -0,0 +1,10 @@
+void main() {
+  // String Interpolation
+
+  var name = "dongmin";
+  var age = 10;
+  var greeting = "Hello everyone, my name is $name and I'm ${age + 2}";
+  var greeting2 = 'Hello everyone, my name is $name and I\'m ${age + 2}';
+  print(greeting);
+  print(greeting2);
+}
