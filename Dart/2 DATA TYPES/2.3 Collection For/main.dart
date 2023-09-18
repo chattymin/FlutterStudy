7bb7b0168a044c38c881dc5cdeafd6610ee117ef
Dongmin <pdm001125@naver.com>
2023-09-18 19:43:02 +0900

    Collection For

---
+++ Dart/2 DATA TYPES/2.3 Collection For/main.dart
@@ -0,0 +1,12 @@
+void main() {
+  // Collection For
+  var oldFriends = ['nico', 'lynn'];
+  var newFriends = [
+    'lewis',
+    'ralph',
+    'darren',
+    for (var friend in oldFriends) "❤️ $friend"
+  ];
+
+  print(newFriends);
+}
