b35d3f1b9899b919a2c3409caf374751b8970138
Dongmin <pdm001125@naver.com>
2023-09-19 11:29:54 +0900

    예시 및 설명 추가

--- Dart/3 FUNCTIONS/3.0 Defining a Function/main.dart
+++ Dart/3 FUNCTIONS/3.0 Defining a Function/main.dart
@@ -2,7 +2,9 @@ String sayHello(String name) {
   return "Hello $name nice to meet you";
 }
 
+// Fat Arrow Syntax : 바로 리턴
 String sayHello2(String name) => "Hello $name nice to meet you";
+num plus(num a, num b) => a + b;
 
 void main() {
   // Defining a Function
