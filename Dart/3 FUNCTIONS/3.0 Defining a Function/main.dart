258e570db8999aa67b0376c0220e714fa801ed38
Dongmin <pdm001125@naver.com>
2023-09-19 11:21:16 +0900

    Defining a Function

---
+++ Dart/3 FUNCTIONS/3.0 Defining a Function/main.dart
@@ -0,0 +1,11 @@
+String sayHello(String name) {
+  return "Hello $name nice to meet you";
+}
+
+String sayHello2(String name) => "Hello $name nice to meet you";
+
+void main() {
+  // Defining a Function
+  print(sayHello("동민"));
+  print(sayHello("동민"));
+}
